e894102f7d6d25397f4e9f80b5e1effdc9034e49
camfairchild <cameron@fairchild.dev>
2022-04-11 22:39:08 -0400

    fix path

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
 });
 
 spdy.createServer({
-    key: fs.readFileSync('/etc/letsencrypt/live/www.example.com/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/www.example.com/fullchain.pem')
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.crt')
 }, app).listen(5000);
