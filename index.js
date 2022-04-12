c85c6a7558f29ea25cbe70529488918335423791
camfairchild <cameron@fairchild.dev>
2022-04-11 22:38:24 -0400

    added certs for https

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import "spdy";
-import "express";
+import spdy from "spdy";
+import express from "express";
+import fs from "fs";
 
 const app = express();
 
@@ -7,4 +8,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-spdy.createServer({}, app).listen(5000);
+spdy.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/www.example.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/www.example.com/fullchain.pem')
+}, app).listen(5000);
